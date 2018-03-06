fdba0f674678292b49ea66d3887c1e3816f32f22
Michele Murgolo <michele007_@hotmail.it>
2018-03-06 10:45:53 +0100

    deleted cleanIndex method

--- docRetrieve.py
+++ docRetrieve.py
@@ -21,20 +21,6 @@ def gatherDocs():
     files = [f for f in listdir(get_path()) if isfile(join(get_path(), f))]
     return files
 
-def cleanIndex(dirname):
-    if not os.path.exists(dirname):
-        os.mkdir(dirname)
-    # Always create the index from scratch
-    ix = create_in(dirname, schema=get_schema())
-    ix = open_dir(dirname)
-    writer = ix.writer()
-
-    # Assume we have a function that gathers the filenames of the
-    # documents to be indexed
-    for path in gather_docs():
-        add_doc(writer, path)
-
-    writer.commit()
 
 def addDoc(writer, path):
     fileobj = open(path, "rb")
